a9b2b53b1f597e2b6df31ed9c655d384f57d6d2f
Tracy Wilson <tlwilson84@gmail.com>
2018-08-06 22:10:25 -0500

    Tournament Eval

--- Project1A.R
+++ Project1A.R
@@ -79,21 +79,24 @@ corrplot(cor(R), order = "hclust")
 
 library(ggplot2)
 
-ggplot(eloDataSm, aes(x=Year, y=Rating, color=Confederation)) +
+ggplot(eloDataSm, aes(x=Year, y=Rating, color=Confederation, fill = Confederation)) +
   geom_point(size=2, shape=23)
 
 eloDataSm2 <- sqldf("SELECT Year, Team , Rating, Confederation   FROM eloData  WHERE Confederation ='CONMEBOL'")
 
 ggplot(eloDataSm2, aes(x=Year, y=Rating, color=Team, fill=Team)) +
-  geom_point(size=2, shape=21)
+  geom_line(size=1, shape=21)
 
 fit <- lm(Year ~ Rating, data=eloDataSm2)
 summary(fit) # show results
 
 
+fit <- lm(Year ~ Rating + Rank + Total + Home + Away + Wins + Losses + Goals.For + Goals.Against, data=eloData)
+summary(fit) # show results
 
 
-#ZXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 library(dplyr)
 matches <- read.csv(file = "WCQFGamesNoFriendlies.csv", head = TRUE, stringsAsFactors = FALSE)
@@ -101,9 +104,9 @@ colnames(matches)
 
 matches1 <- matches
 
-matches %>%select(Date, Home_Team, Away_Team, Home_Team_Goals, Away_Team_Goals) %>%
-  mutate(Total.Goals = Home_Team_Goals + Away_Team_Goals) %>%
-  arrange(-Total.Goals) %>%
+matches %>% select(Date, Home_Team, Away_Team, Home_Team_Goals, Away_Team_Goals) %>%
+  mutate(Total_Goals = Home_Team_Goals + Away_Team_Goals) %>%
+  arrange(-Total_Goals) %>%
   head(1)
 
 teams <- data.frame(team = unique(c(matches$Home_Team, matches$Away_Team)))
@@ -195,6 +198,48 @@ elo.prob(brazil, iceland)
 
 #This way, you can simulate the entire competition all the way from the group stage to the final. And if you repeat this process many (thousands of) times, you will get detailed probabilities for each team to make it to the each stage of the competition. This is essentially what websites like FiveThirtyEight do for their sport predictions, with probabilities based on 100,000 simulations of the rest of the season.
 
+
+make_tournament_dataset <- function(seed = NULL)
+{
+  set.seed(seed)
+
+  all.teams <- c("Russia", "Germany", "Brazil", "Portugal", "Argentina", "Belgium",
+                 "Poland", "France", "Spain", "Peru", "Switzerland", "England",
+                 "Colombia", "Mexico", "Uruguay", "Croatia", "Denmark", "Iceland",
+                 "Costa Rica", "Sweden", "Tunisia", "Egypt", "Senegal", "Iran",
+                 "Serbia", "Nigeria", "Australia", "Japan", "Morocco", "Panama",
+                 "South Korea", "Saudi Arabia")
+  means <- c(18, 12, 17, 13, 16, 15.5, 14, 14.5)
+  names(means) <- all.teams
+
+  tournament <- expand.grid(team.Home = all.teams, team.Visitor = all.teams, stringsAsFactors = FALSE)
+  tournament <- tournament[tournament$team.Home != tournament$team.Visitor, ]
+
+  tournament$points.Home <- vapply(means[tournament$team.Home] + 3, stats::rpois, 0, n = 1)
+  tournament$points.Visitor <- vapply(means[tournament$team.Visitor], stats::rpois, 0, n = 1)
+
+  tournament$week <- 0
+  tournament$week[1] <- 1
+  for(i in 2:nrow(tournament))
+  {
+    t1 <- tournament$team.Home[i]
+    t2 <- tournament$team.Visitor[i]
+    idx <- 1:(i-1)
+    wks <- tournament$week[idx]
+    tm1 <- tournament$team.Home[idx]
+    tm2 <- tournament$team.Visitor[idx]
+
+    tournament$week[i] <- min(setdiff(1:14, wks[tm1 %in% c(t1, t2) | tm2 %in% c(t1, t2)]))
+  }
+
+  tournament$half <- ifelse(tournament$week < 8, "First Half of Season", "Second Half of Season")
+  tournament <- tournament[order(tournament$week), ]
+  rownames(tournament) <- NULL
+  attr(tournament, "out.attrs") <- NULL
+
+  tournament
+}
+
 WC_teams1 <- subset(matches1, Home_Team == c("Russia", "Germany", "Brazil", "Portugal", "Argentina", "Belgium",
                                              "Poland", "France", "Spain", "Peru", "Switzerland", "England",
                                              "Colombia", "Mexico", "Uruguay", "Croatia", "Denmark", "Iceland",
@@ -204,3 +249,7 @@ WC_teams1 <- subset(matches1, Home_Team == c("Russia", "Germany", "Brazil", "Por
 
 
 as.matrix(elo.run(score(Home_Team_Goals , Away_Team_Goals) ~ Home_Team + Away_Team, data = WC_teams1, k = 20))
+
+library(elo)
+help(make_tournament_dataset)
+
